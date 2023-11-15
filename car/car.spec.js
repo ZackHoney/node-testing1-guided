c21cad0824f96f41748ecbeb72f1a4cab797bff2
Zack Honey <zhoney466@gmail.com>
2023-11-14 22:30:09 -0800

    wip

--- car/car.spec.js
+++ car/car.spec.js
@@ -3,4 +3,14 @@ describe('our first test', () => {
     test('sanity', () => {
         expect(5).toBe(5)
     })
+    test('objects', () => {
+        const o = { a: 1 }
+        const oo = { a: 1 }
+        const ooo = oo
+        expect(o).toBe(o)
+        expect(oo).toBe(ooo)
+    })
+    test('objects again', () => {
+        expect({ a: 1 }).toEqual({ a: 1 })
+    })
 })
